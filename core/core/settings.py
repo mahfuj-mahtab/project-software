db87f840e9ba3bc97cd4b82719373b8d44740a64
Mahfuj Mahtab Mohot <mohotmohot1@gmail.com>
2024-10-11 08:48:24 +0600

    project model relations fixed and django rest framework installed

--- core/core/settings.py
+++ core/core/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "apps.project"
+    "apps.project",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
